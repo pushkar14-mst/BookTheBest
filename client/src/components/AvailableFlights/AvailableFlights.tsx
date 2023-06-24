b4e6ff5f63f3d298c2e878b9bd518915fa7140a9
Pushkar Patil <pushkar14.patil@gmail.com>
2023-06-24 21:53:11 +0530

    started designing offer tile

--- client/src/components/AvailableFlights/AvailableFlights.tsx
+++ client/src/components/AvailableFlights/AvailableFlights.tsx
@@ -3,7 +3,23 @@ const AvailableFlights: React.FC = () => {
   return (
     <>
       <div className="flights-offers-container">
-        <div className="offers-tile"></div>
+        <div className="offers-tile">
+          <div className="one-way-offer">
+            <div className="arrival-info">
+              <h1>BOM</h1>
+              <p>17:00</p>
+            </div>
+            <div className="flight-time">
+              <div className="flight-price">
+                <h3>53,000RS</h3>
+              </div>
+            </div>
+            <div className="departure-info">
+              <h1>SFO</h1>
+              <p>10:50</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
